99cd1f2e8b34e1b6db8a16084387113fd1496c3c
Pedro Cabrera <pedro.c.vfx@gmail.com>
2025-05-06 19:10:37 +0200

    updated build and built for 20.5.487 py3.10

--- windowsbuild.bat
+++ windowsbuild.bat
@@ -1,6 +1,9 @@
+rmdir /s /q build
+mkdir ./build
 cd ./build
-set HOUDINI_PATH=C:\Program Files\Side Effects Software\Houdini 20.5.278\toolkit\cmake
-set NVFLEX_DIR=F:\ENVIRONMENTS\FLEX_LIB\flex_1.1
+set HOUDINI_PATH=C:\Program Files\Side Effects Software\Houdini 20.5.487\toolkit\cmake
+set NVFLEX_DIR=C:\Users\pedro\Desktop\Houdini-NvFlex\FLEX_LIB\flex_1.1
+
 
 cmake -DCMAKE_PREFIX_PATH="%HOUDINI_PATH%" -DNVFLEX_DIR="%NVFLEX_DIR%" -G "Visual Studio 16 2019" -A x64 ..
 cmake --build . --config Release
